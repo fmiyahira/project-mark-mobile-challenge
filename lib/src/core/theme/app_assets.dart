b98954f083d5c5a5fec177d1738ce4e5c590525f
Fernando Augusto Miyahira <miyahirafernando@gmail.com>
2025-04-12 13:44:14 -0400

    feat: add design system

---
+++ lib/src/core/theme/app_assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  static const String sunnyIcon = 'assets/images/icons/sunny_outline.svg';
+  static const String cloudyIcon = 'assets/images/icons/cloudy_outline.svg';
+  static const String rainyIcon = 'assets/images/icons/rainy_outline.svg';
+  static const String lightningIcon =
+      'assets/images/icons/lightning_outline.svg';
+
+  static const String cloudBackground = 'assets/images/backgrounds/cloud.svg';
+  static const String sunBackground = 'assets/images/backgrounds/sun.svg';
+
+  static const String projectmarkLogo = 'assets/images/logos/projectmark.svg';
+}
---
+++ lib/src/core/theme/app_text_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle headline1 = TextStyle(
+    fontFamily: 'Archivo',
+    fontWeight: FontWeight.w800,
+    fontSize: 42,
+  );
+
+  static const TextStyle headline2 = TextStyle(
+    fontFamily: 'Archivo',
+    fontWeight: FontWeight.w800,
+    fontSize: 28,
+  );
+
+  static const TextStyle subtitle1 = TextStyle(
+    fontFamily: 'Archivo',
+    fontWeight: FontWeight.w800,
+    fontSize: 21,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontFamily: 'Archivo',
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+  );
+
+  static const TextStyle bodyText2 = TextStyle(
+    fontFamily: 'Archivo',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+}
